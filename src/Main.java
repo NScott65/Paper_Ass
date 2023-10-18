edbc2939e3290cced5bd9f6ad67bc406aa9a7cb1
Noah <scott.613865@swl.k12.oh.us>
2023-10-18 09:06:26 -0400

    hi

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        boolean cont = false;
+        String contYN;
+        String word;
+        int random;
+
+        System.out.println("Enter a word or phase: ");
+        word = scan.nextLine();
+
+        if(word.length() > 1){
+            do{
+                random = (int)(Math.random() * (word.length() + 1));
+                word = word.substring(0, random) + word.substring(random + 1);
+                System.out.println("Your new word/phrase is: " + word + ".");
+                if(word.length() > 1){
+                    System.out.println("Again?[Y/N] ");
+                    contYN = scan.nextLine();
+                    if(contYN.equalsIgnoreCase( "N")){
+                        cont = true;
+                    }
+                }else{
+                    System.out.println("Your word/phrase can't be smaller.");
+                    cont = true;
+                }
+            }while(!cont);
+
+        }else{
+            System.out.println("Your word/phase is too small to continue.");
+        }
+    }
+}
